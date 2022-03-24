676cc7429d5f55338f64bc14515244e62bab0eff
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2022-03-24 15:28:49 +0530

    interface for communicating with the Flask API

---
+++ src/service/pokerAIService.js
@@ -0,0 +1,38 @@
+const axios = require('axios').default;
+
+// FIXME: the endpoint should come from environment variables
+const FLASK_ENDPOINT = "http://127.0.0.1:5000/"
+
+const ping = async () => {
+	return await axios.get(FLASK_ENDPOINT)
+		.then((response) => {
+			if (response.status == 200) {
+				console.info(`[pokerAIService] ping: ${response.data}`);
+				return true;
+			}
+			else {
+				return false;
+			}
+		})
+		.catch((err) => {
+			console.error(`[pokerAIService] Ping failed`, err);
+			return false;
+		})
+}
+
+const getAIMove = async (level, game, hand, playOptions, botId) => {
+	return await axios.post(`${FLASK_ENDPOINT}bot/${level}`, { botId, game, hand, playOptions })
+		.then((response) => {
+			return response.data;
+		})
+		.catch((err) => {
+			console.error(`[pokerAIService] getAIMove error`, err)
+			throw err
+		})
+
+}
+
+module.exports = {
+	ping,
+	getAIMove,
+}
