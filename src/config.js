52f3a33427782e8c96fe9521d34b85d2c07ac277
Sarvesh Raj <sarvesh0803@gmail.com>
2022-03-28 17:35:48 +0530

    deployed to app engine

--- src/config.js
+++ src/config.js
@@ -6,9 +6,10 @@ const local = {
 }
 
 const prod = {
-    mongoURI: process.env.mongoURI,
-    jwtPrivateKey: process.env.jwtPrivateKey,
-    encryptionSalt: process.env.encryptionSalt
+    // mongoURI: process.env.mongoURI,
+    mongoURI: 'mongodb://megapokerbot:mega123@cluster0-shard-00-00.vpwpq.mongodb.net:27017,cluster0-shard-00-01.vpwpq.mongodb.net:27017,cluster0-shard-00-02.vpwpq.mongodb.net:27017/poker-db?atlas-hugr40-shard-0&ssl=true&authSource=admin',
+    jwtPrivateKey: 'z&234!jsh9,',
+    encryptionSalt: 'encryptionSalt'
 }
 
 module.exports = process.env.NODE_ENV === 'production' ? prod : local
